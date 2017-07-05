b6639fb3a7daae243999298409b75a9d4383706c
Grant Visser <grantrvisser@gmail.com>
2017-07-05 16:44:27 +0200

    Single Threaded Implementation

---
+++ Singleton Pattern Example/Singleton Pattern Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton testSingleton = Singleton.GetInstance();
+            Console.Out.WriteLine(testSingleton.GetDescription());
+            Console.Out.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton Pattern Example/Singleton Pattern Example/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern_Example
+{
+    //Good lord. This is not thread safe.
+    class Singleton
+    {
+        private static Singleton uniqueSingleton;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (uniqueSingleton == null)
+            {
+                uniqueSingleton= new Singleton();
+            }
+            return uniqueSingleton;
+        }
+
+        public string GetDescription()
+        {
+            return "I'm a singleton";
+        }
+    }
+}
